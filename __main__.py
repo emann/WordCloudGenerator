5a483b93489da4051bcbdbd3bc37356a862aac3a
Ethan Mann <emannguitar@gmail.com>
2019-11-17 23:37:06 -0500

    Added filtering for links and 'RT'.

--- __main__.py
+++ __main__.py
@@ -1,19 +1,23 @@
 import time
+import re
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
-from interfaces import WordStringRequestConfig, DataInterfaceManager
+from interfaces import WordListRequestConfig, DataInterfaceManager
 from config import API_KEYS
 
 dim = DataInterfaceManager(API_KEYS)
+STOPWORDS.add('RT')
+
 
 if __name__ == '__main__':
-    word_list = dim.request_word_string(WordStringRequestConfig('twitter', 'hashtag', 'impeachmentinquiry', 1000, None, 'top', None))
-    print(word_list)
+    word_list = dim.request_word_list(WordListRequestConfig('twitter', 'hashtag', 'impeachmentinquiry', 100, None, 'top', None))
+    word_list = filter(lambda x: not re.search(r'^https', x), word_list)
+    word_string = ' '.join(word_list)
     wordcloud = WordCloud(width=800, height=800,
                           background_color='white',
                           stopwords=set(STOPWORDS),
-                          min_font_size=10).generate(word_list)
+                          min_font_size=10).generate(word_string)
     plt.figure(figsize=(8, 8), facecolor=None)
     plt.imshow(wordcloud)
     plt.axis("off")
